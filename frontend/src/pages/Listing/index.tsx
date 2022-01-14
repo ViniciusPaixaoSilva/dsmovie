3e5f1aa001f639b4b1283d7640dc265f392536fe
Vinicius Paixao Silva <viniciuspaixao001@gmail.com>
2022-01-14 11:17:34 -0300

    usesState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,34 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movies";
 import { BASE_URL } from "utils/request";
 
 function Listing() {
 
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=2&page=0`)
+      .then(response => {
+        const data = response.data as MoviePage;
+        console.log(data);
+        setPageNumber(data.number);
+      });
+  }, []);
+
   // FORMA ERRADA
+  /*
   axios.get(`${BASE_URL}/movies?size=2&page=0`)
       .then(response => {
-        console.log(response.data);
+        const data = response.data as MoviePage;
+        setPageNumber(data.number);
       });
-
+  */
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
